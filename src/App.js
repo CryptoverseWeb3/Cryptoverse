760b19b32416ad928e5822091b355fccc1f877e1
kabir0x17 <kabirj2310@gmail.com>
2022-04-26 15:30:12 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+
+const App = () => {
+    return (
+        <div className={"app"}>
+            <div className={"navbar"}>
+
+            </div>
+
+            <div className={"main"}>
+
+            </div>
+
+            <div className={"footer"}>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, Menu, Typography, Avatar} from 'antd';
+import {Link} from 'react-router-dom';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from "@ant-design/icons";
+
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Navbar;
