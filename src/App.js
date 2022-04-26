e2e21a38e93e5ed6686f66ac9445244ea5578daa
kabir0x17 <kabirj2310@gmail.com>
2022-04-26 22:44:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 
+import {Navbar} from './components';
+import './App.css';
+
 const App = () => {
     return (
         <div className={"app"}>
             <div className={"navbar"}>
-
+                <Navbar/>
             </div>
 
             <div className={"main"}>
