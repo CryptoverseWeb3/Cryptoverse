f1f2dfdebb5a6294b5c793198590c5d771ad1da3
kabir0x17 <kabirj2310@gmail.com>
2022-04-27 21:24:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 
-import {Navbar} from './components';
+import {Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News} from './components';
 import './App.css';
 
 const App = () => {
@@ -13,14 +13,38 @@ const App = () => {
             </div>
 
             <div className={"main"}>
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path={"/"}>
+                                <Homepage/>
+                            </Route>
 
+                            <Route exact path={"/exchanges"}>
+                                <Exchanges/>
+                            </Route>
+
+                            <Route exact path={"/cryptocurrencies"}>
+                                <Cryptocurrencies/>
+                            </Route>
+
+                            <Route exact path={"/crypto/:coinId"}>
+                                <CryptoDetails/>
+                            </Route>
+                            <Route exact path={"/nes"}>
+                                <News/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
             </div>
 
             <div className={"footer"}>
 
             </div>
         </div>
-    );
+    )
+        ;
 }
 
 export default App;
