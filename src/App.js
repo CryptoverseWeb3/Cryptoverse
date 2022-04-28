dfc5fbef57d83d78f646540abdd2bc6ea1f47467
kabir0x17 <kabirj2310@gmail.com>
2022-04-28 20:06:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 
 import {Navbar} from './components';
@@ -13,12 +13,38 @@ const App = () => {
             </div>
 
             <div className={"main"}>
+<<<<<<< Updated upstream
 
             </div>
 
             <div className={"footer"}>
+=======
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path={"/"} element={<Homepage/>}/>
+                            <Route exact path={"/exchanges"} element={<Exchanges/>}/>
+                            <Route exact path={"/cryptocurrencies"} element={<Cryptocurrencies/>}/>
+                            <Route exact path={"/crypto/:coinId"} element={<CryptoDetails/>}/>
+                            <Route exact path={"/news"} element={<News/>}/>
+                        </Routes>
+                    </div>
+                </Layout>
+>>>>>>> Stashed changes
 
+                <div className={"footer"}>
+                    <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                        Cryptoverse <br/>
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to={"/"}>Home</Link>
+                        <Link to={"/exchanges"}>Exchanges</Link>
+                        <Link to={"/news"}>News</Link>
+                    </Space>
+                </div>
             </div>
+
         </div>
     );
 }
