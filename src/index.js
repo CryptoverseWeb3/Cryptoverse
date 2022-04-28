c7b18132c6861d11bb5276e5fd20d59d40c98252
kabir0x17 <kabirj2310@gmail.com>
2022-04-28 20:07:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,3 +13,5 @@ root.render(
         </Router>
     </React.StrictMode>
 );
+
+
