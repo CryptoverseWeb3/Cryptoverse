54b096b574a882463b519b292e918b72614fcfa8
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-02 09:45:59 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 import App from './App';
+import store from './app/store';
 import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
-            <App/>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </Router>
     </React.StrictMode>
 );
