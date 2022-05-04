a840a0f1c6ee9293a6ee5dbbefc9c975de83aaa7
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-04 12:03:15 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import millify from 'millify';
+import {Typography, Row, Col, Statistic} from "antd";
+import {Link} from 'react-router-dom';
+
+import {useGetCryptosQuery} from '../services/cryptoApi';
+
+const {Title} = Typography;
 
 const Homepage = () => {
+    const {data, isFetching} = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+    if (isFetching) {
+        return 'Loading...'
+    }
+
+
     return (
-        <div>
-            Homepage
-        </div>
-    );
-};
+        <>
+            <Title level={2} className={"heading"}>Global Crypto Stats</Title>
+
+            <Row>
+                <Col span={12}><Statistic title={"Total Cryptocurrencies"} value={globalStats.total}/></Col>
+                <Col span={12}><Statistic title={"Total Exchanges"} value={millify(globalStats.totalExchanges)}/></Col>
+                <Col span={12}><Statistic title={"Total Market Cap"} value={millify(globalStats.totalMarketCap)}/></Col>
+                <Col span={12}><Statistic title={"Total 24h Volume"} value={millify(globalStats.total24hVolume)}/></Col>
+                <Col span={12}><Statistic title={"Total Markets"} value={millify(globalStats.totalMarkets)}/></Col>
+            </Row>
+        </>
+    )
+}
 
 export default Homepage;
