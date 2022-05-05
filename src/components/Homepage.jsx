3e137c4f6818b3961b2b307fdd0fd96b574f9100
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-05 14:04:48 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,11 +4,12 @@ import {Typography, Row, Col, Statistic} from "antd";
 import {Link} from 'react-router-dom';
 
 import {useGetCryptosQuery} from '../services/cryptoApi';
+import {Cryptocurrencies, News} from "../components";
 
 const {Title} = Typography;
 
 const Homepage = () => {
-    const {data, isFetching} = useGetCryptosQuery();
+    const {data, isFetching} = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
     if (isFetching) {
         return 'Loading...'
@@ -26,6 +27,17 @@ const Homepage = () => {
                 <Col span={12}><Statistic title={"Total 24h Volume"} value={millify(globalStats.total24hVolume)}/></Col>
                 <Col span={12}><Statistic title={"Total Markets"} value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className={"home-title"}>Top 10 Cryptocurrencies in the world</Title>
+                <Title level={2} className={"home-title"}><Link to={"/cryptocurrencies"}>Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified/>
+            <div className="home-heading-container">
+                <Title level={2} className={"home-title"}>Latest Crypto News</Title>
+                <Title level={2} className={"home-title"}><Link to={"/news"}>Show more</Link></Title>
+            </div>
+            <News simplified/>
+
         </>
     )
 }
