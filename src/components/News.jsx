35da3b9576eebf010c31c2265e61f11653dfc1dc
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-07 12:25:10 +0530

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import {Select, Typography, Row, Col, Avatar, Card} from "antd";
+import moment from 'moment';
 
-const News = () => {
+import {useGetCryptoNewsQuery} from "../services/cryptoNewsApi";
+
+const {Text, Title} = Typography;
+const {Option} = Select;
+
+
+const News = ({simplified}) => {
+    const {data: cryptoNews} = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12});
+
+    if (!cryptoNews?.value) return 'Loading...'
+
     return (
-        <div>
-            News
-        </div>
+        <Row
+            gutter={[24, 24]}>
+            {cryptoNews.value.map((news, i) => (
+                <Col xs={24} sm={12} lg={8} key={i}>
+                    <Card hoverable className={"news-card"}>
+                        <a href={news.url} target={"_blank"} rel={"noreferrer"}>
+                            <div className="news-image-container">
+                                <Title className={"news-title"} level={4}>{news.name}</Title>
+                            </div>
+
+                        </a>
+
+                    </Card>
+
+                </Col>
+            ))}
+        </Row>
     );
 };
 
