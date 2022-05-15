16d14fa452b2afae395b9a8e426126f2f2dd1dc7
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-15 13:12:39 +0530

    Update CryptoDetails.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,19 +1,9 @@
 import React, {useState} from 'react';
-// import HTMLReactParser from 'html-react-parser';
+import HTMLReactParser from 'html-react-parser';
 import {useParams} from 'react-router-dom';
 import millify from 'millify';
 import {Col, Row, Typography, Select} from 'antd';
-import {
-    MoneyCollectOutlined,
-    DollarCircleOutlined,
-    FundOutlined,
-    ExclamationCircleOutlined,
-    StopOutlined,
-    TrophyOutlined,
-    CheckOutlined,
-    NumberOutlined,
-    ThunderboltOutlined
-} from '@ant-design/icons';
+import {MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined} from '@ant-design/icons';
 
 import {useGetCryptoDetailsQuery} from '../services/cryptoApi';
 // import Loader from './Loader';
@@ -34,20 +24,20 @@ const CryptoDetails = () => {
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
     const stats = [
-        {title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined/>},
-        {title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined/>},
-        {title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined/>},
-        {title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined/>},
-        {title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined/>},
+        { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
+        { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+        { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
+        { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
+        { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
     ];
 
-    // const genericStats = [
-    //     {title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined/>},
-    //     {title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined/>},
-    //     {title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined/> : <StopOutlined/>, icon: <ExclamationCircleOutlined/>},
-    //     {title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined/>},
-    //     {title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined/>},
-    // ];
+    const genericStats = [
+        { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
+        { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+        { title: 'Aprroved Supply', value: cryptoDetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+        { title: 'Total Supply', value: `$ ${cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)}`, icon: <ExclamationCircleOutlined /> },
+        { title: 'Circulating Supply', value: `$ ${cryptoDetails?.supply?.circulating && millify(cryptoDetails?.supply?.circulating)}`, icon: <ExclamationCircleOutlined /> },
+    ];
 
     return (
         <Col className={"coin-detail-container"}>
@@ -82,10 +72,55 @@ const CryptoDetails = () => {
                                 <Text>{icon}</Text>
                                 <Text>{title}</Text>
                             </Col>
-                                <Text className={"stats"}>{value}</Text>
+                            <Text className={"stats"}>{value}</Text>
                         </Col>
 
                     ))}
+                </Col>
+
+                <Col className={"other-stats-info"}>
+                    <Col className={"coin-value-statistics-heading"}>
+                        <Title level={3} className="coin-details-heading">
+                            Other Statistics
+                        </Title>
+                        <p>
+                            An overview showing the stats of all cryptocurrencies
+                        </p>
+                    </Col>
+                    {genericStats.map(({icon, title, value}) => (
+                        <Col className={"coin-stats"}>
+                            <Col className={"coin-stats-name"}>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className={"stats"}>{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
+
+            <Col className={"coin-desc-link"}>
+                <Row className={"coin-desc"}>
+                    <Title level={3} className={"coin-details-heading"}>
+                        What is {cryptoDetails.name}
+                        {HTMLReactParser(cryptoDetails.description)}
+                    </Title>
+                </Row>
+                <Col className={"coin-links"}>
+                    <Title level={3} className={"coin-details-heading"}>
+                        {cryptoDetails.name} Links
+                        {cryptoDetails.links.map((link) => (
+                            <Row className={"coin-link"} key={link.name}>
+                                <Title level={5} className={"link-name"}>
+                                    {link.type}
+                                </Title>
+                                <a href={link.url} target={"_black"} rel={"noreferrer"}>
+                                    {link.name}
+                                </a>
+                            </Row>
+                        ))}
+                    </Title>
+
                 </Col>
             </Col>
 
