1348da3bd8bfa694107ba8162484f141a1d582d7
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-16 08:09:31 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to={"/"}>Cryptoverse</Link>
                 </Typography.Title>
-                <Button className={"menu-control-container"} onClick={() => setActiveMenu(true)}>
+                <Button className={"menu-control-container"} onClick={() => setActiveMenu(!activeMenu)}>
                 <MenuOutlined />
                 </Button>
             </div>
