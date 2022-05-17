3143a74ed8ee04215d68404f900dff5cb782b94e
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-17 09:12:31 +0530

    Update CryptoDetails.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -6,7 +6,7 @@ import {Col, Row, Typography, Select} from 'antd';
 import {MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined} from '@ant-design/icons';
 
 import {useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} from '../services/cryptoApi';
-// import Loader from './Loader';
+import Loader from './Loader';
 import LineChart from './LineChart';
 
 const {Title, Text} = Typography;
@@ -19,7 +19,7 @@ const CryptoDetails = () => {
     const {data: coinHistory} = useGetCryptoHistoryQuery({coinId, timeperiod});
     const cryptoDetails = data?.data?.coin;
 
-    if (isFetching) return "<Loader />";
+    if (isFetching) return <Loader />;
 
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
