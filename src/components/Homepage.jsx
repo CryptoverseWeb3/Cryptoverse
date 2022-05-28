4e877bedf91b0ff7ab8221126fff860018cd5f08
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-28 20:03:45 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -28,6 +28,7 @@ const Homepage = () => {
                 <Col span={12}><Statistic title={"Total 24h Volume"} value={millify(globalStats.total24hVolume)}/></Col>
                 <Col span={12}><Statistic title={"Total Markets"} value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
+
             <div className="home-heading-container">
                 <Title level={2} className={"home-title"}>Top 10 Cryptocurrencies in the world</Title>
                 <Title level={2} className={"home-title"}><Link to={"/cryptocurrencies"}>Show more</Link></Title>
