c7fd1ed5ab51f4e759a82b371a67652f8341117f
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-02 22:22:18 +0530

    Update Exchanges.jsx

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import millify from 'millify';
+// import millify from 'millify';
 // import { Collapse, Row, Col, Typography, Avatar } from 'antd';
 // import HTMLReactParser from 'html-react-parser';
 
 // import { useGetExchangesQuery } from '../services/cryptoApi';
-import Loader from './Loader';
+// import Loader from './Loader';
 
 // const { Text } = Typography;
 // const { Panel } = Collapse;
