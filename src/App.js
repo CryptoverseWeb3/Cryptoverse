bc3f4568e69e7663027894557bf2d9c594471991
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-08 23:48:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import {Layout, Typography, Space} from 'antd';
 import {Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News} from './components';
 import './App.css';
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
-
 const App = () => {
     return (
         <div className={"app"}>
@@ -18,30 +15,28 @@ const App = () => {
             <div className={"main"}>
                 <Layout>
                     <div className="routes">
-                        <BrowserRouter basename="/cryptoverse">
-                            <Routes basename="/cryptoverse">
-                                <Route exact path={"/"} element={<Homepage/>}/>
-                                <Route exact path={"/exchanges"} element={<Exchanges/>}/>
-                                <Route exact path={"/cryptocurrencies"} element={<Cryptocurrencies/>}/>
-                                <Route exact path={"/crypto/:coinId"} element={<CryptoDetails/>}/>
-                                <Route exact path={"/news"} element={<News/>}/>
-                            </Routes>
-                        </BrowserRouter>
+                        <Routes>
+                            <Route exact path={"/"} element={<Homepage/>}/>
+                            <Route exact path={"/exchanges"} element={<Exchanges/>}/>
+                            <Route exact path={"/cryptocurrencies"} element={<Cryptocurrencies/>}/>
+                            <Route exact path={"/crypto/:coinId"} element={<CryptoDetails/>}/>
+                            <Route exact path={"/news"} element={<News/>}/>
+                        </Routes>
                     </div>
                 </Layout>
             </div>
 
-            <div className={"gradient-bg-footer"}>
-                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
-                    Cryptoverse <br/>
-                    All rights reserved
-                </Typography.Title>
-                <Space>
-                    <Link to={"/"}>Home</Link>
-                    <Link to={"/exchanges"}>Exchanges</Link>
-                    <Link to={"/news"}>News</Link>
-                </Space>
-            </div>
+                <div className={"gradient-bg-footer"}>
+                    <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                        Cryptoverse <br/>
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to={"/"}>Home</Link>
+                        <Link to={"/exchanges"}>Exchanges</Link>
+                        <Link to={"/news"}>News</Link>
+                    </Space>
+                </div>
 
         </div>
     );
