036a97b955460b192c206a105d02ca0a6338fea6
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-09 21:15:49 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,23 @@ const Footer = () => {
                 Cryptoverse <br/>
                 All rights reserved
             </Typography.Title>
-            <Space>
-                <Link className={"text-white"} to='/'>Home</Link>
-                <Link className={"text-white"} to='/cryptocurrencies'>Cryptocurrencies</Link>
-                <Link className={"text-white"} to='/news'>News</Link>
-                <Link className={"text-white"} to='/Wallets'>Wallets</Link>
-                <Link className={"text-white"} to='/Tutorials'>Tutorials</Link>
-            </Space>
+
+            {/*<Space>*/}
+            {/*    <Link className={"text-white"} to='/'>Home</Link>*/}
+            {/*    <Link className={"text-white"} to='/cryptocurrencies'>Cryptocurrencies</Link>*/}
+            {/*    <Link className={"text-white"} to='/news'>News</Link>*/}
+            {/*    <Link className={"text-white"} to='/Wallets'>Wallets</Link>*/}
+            {/*    <Link className={"text-white"} to='/Tutorials'>Tutorials</Link>*/}
+            {/*</Space>*/}
+            <div className={"text-white md:flex hidden list-none flex-row justify-between items-center  text-white-600 hover:text-blue-800 visited:text-white-600 text-2xl"}>
+                <a className={"text-white"} href="https://twitter.com/CryptoverseWeb3"><i className="social-icons ri-twitter-line"></i></a>
+                <a className={"text-white"} href="https://github.com/CryptoverseWeb3"><i className="social-icons ri-github-fill"></i></a>
+                <a className={"text-white"} href="https://www.youtube.com/channel/UCu7EGHHiNgEN2228BKD1pqg/?sub_confirmation=1"><i className="social-icons ri-youtube-fill"></i></a>
+                <a className={"text-white"} href="https://instagram.com/CryptoverseWeb3"><i className="social-icons ri-instagram-fill"></i></a>
+                <a className={"text-white"} href="https://discord.gg/WhDry9FWE7"><i className="social-icons ri-discord-fill"></i></a>
+                <a className={"text-white"} href="https://t.me/cryptoverseweb3"><i className="social-icons ri-telegram-line"></i></a>
+                <a className={"text-white"} href="https://www.facebook.com/cryptoverseweb3"><i className="social-icons ri-facebook-fill"></i></a>
+            </div>
         </div>
     );
 };
