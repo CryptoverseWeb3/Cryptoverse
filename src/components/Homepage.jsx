c4bfe7b521b6017d00483896dbac759e2cfc74a0
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-09 10:27:45 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -19,15 +19,16 @@ const Homepage = () => {
 
     return (
         <>
-            <Title level={2} className={"heading"}>Global Crypto Stats</Title>
-            <Row>
-                <Col span={12}><Statistic title={"Total Cryptocurrencies"} value={globalStats.total}/></Col>
-                <Col span={12}><Statistic title={"Total Exchanges"} value={millify(globalStats.totalExchanges)}/></Col>
-                <Col span={12}><Statistic title={"Total Market Cap"} value={millify(globalStats.totalMarketCap)}/></Col>
-                <Col span={12}><Statistic title={"Total 24h Volume"} value={millify(globalStats.total24hVolume)}/></Col>
-                <Col span={12}><Statistic title={"Total Markets"} value={millify(globalStats.totalMarkets)}/></Col>
-            </Row>
-
+            <div className={"dark-global-crypto-stats"}>
+                <Title level={2} className={"heading"}>Global Crypto Stats</Title>
+                <Row>
+                    <Col span={12}><Statistic title={"Total Cryptocurrencies"} value={globalStats.total}/></Col>
+                    <Col span={12}><Statistic title={"Total Exchanges"} value={millify(globalStats.totalExchanges)}/></Col>
+                    <Col span={12}><Statistic title={"Total Market Cap"} value={millify(globalStats.totalMarketCap)}/></Col>
+                    <Col span={12}><Statistic title={"Total 24h Volume"} value={millify(globalStats.total24hVolume)}/></Col>
+                    <Col span={12}><Statistic title={"Total Markets"} value={millify(globalStats.totalMarkets)}/></Col>
+                </Row>
+            </div>
             <div className="home-heading-container">
                 <Title level={2} className={"home-title"}>Top 10 Cryptocurrencies in the world</Title>
                 <Title level={2} className={"home-title"}><Link to={"/cryptocurrencies"}>Show more</Link></Title>
