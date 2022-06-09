21409b96be4ab06ffae99aa3c1a331543fb53f87
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-09 15:09:47 +0530

    Update CryptoDetails.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -43,7 +43,7 @@ const CryptoDetails = () => {
         {title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined/>},
         {
             title: '24h Volume',
-            value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
+            value: `₹ ${cryptoDetails['24hVolume'] && millify(cryptoDetails['24hVolume'])}`,
             icon: <ThunderboltOutlined/>
         },
         {
