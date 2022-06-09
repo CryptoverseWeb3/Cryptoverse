9f669cb6c1675d0ed0a97b0ffee6364629954304
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-09 08:58:19 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Space, Typography} from "antd";
+import {Link} from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div>
+            <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                Cryptoverse <br/>
+                All rights reserved
+            </Typography.Title>
+            <Space>
+                <Link className={"text-white"} to='/'>Home</Link>,
+                <Link className={"text-white"} to='/cryptocurrencies'>Cryptocurrencies</Link>,
+                <Link className={"text-white"} to='/news'>News</Link>,
+                <Link className={"text-white"} to='/Wallets'>Wallets</Link>,
+                <Link className={"text-white"} to='/Tutorials'>Tutorials</Link>
+            </Space>
+        </div>
+    );
+};
+
+export default Footer;
