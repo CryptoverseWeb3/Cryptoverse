3e4c4369324cffb30362920e663dbb1b5e8dbd20
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-19 17:06:05 +0530

    cryptoverse

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import 'antd/dist/antd.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Router basename={"/cryptoverse"}>
+        <Router>
             <Provider store={store}>
                 <App/>
             </Provider>
