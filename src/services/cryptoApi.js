de3b88b510a3d5a1ffd6e5ad4ad620ca6e1c2ac7
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-19 20:56:59 +0530

    update

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -4,10 +4,14 @@ const RAPIDAPI_HOST = process.env.REACT_APP_RAPIDAPI_HOST;
 const RAPIDAPI_KEY = process.env.REACT_APP_RAPIDAPI_KEY;
 
 const cryptoApiHeaders = {
+    // 'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    // 'X-RapidAPI-Key': 'b1d57f69b3mshadfcecfc9e489a6p14b18cjsn87ec366e56b2'
+
     'X-RapidAPI-Host': RAPIDAPI_HOST,
     'X-RapidAPI-Key': RAPIDAPI_KEY
 }
 
+// const baseUrl = CRYPTO_API_URL;
 const baseUrl = `https://${RAPIDAPI_HOST}/`;
 
 const creatRequest = (url) => ({url, headers: cryptoApiHeaders});
