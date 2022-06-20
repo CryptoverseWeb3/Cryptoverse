3330d25303f92e644fb8cf86f1e688fa42abc073
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-20 19:42:24 +0530

    api

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,15 +1,15 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-const NEWS_RAPIDAPI_HOST = process.env.REACT_APP_NEWS_RAPIDAPI_HOST;
-const NEWS_RAPIDAPI_KEY = process.env.REACT_APP_NEWS_RAPIDAPI_KEY;
+// const NEWS_RAPIDAPI_HOST = process.env.REACT_APP_NEWS_RAPIDAPI_HOST;
+// const NEWS_RAPIDAPI_KEY = process.env.REACT_APP_NEWS_RAPIDAPI_KEY;
 
 const cryptoNewsApiHeaders = {
     'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Host': NEWS_RAPIDAPI_HOST,
-    'X-RapidAPI-Key': NEWS_RAPIDAPI_KEY
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'b1d57f69b3mshadfcecfc9e489a6p14b18cjsn87ec366e56b2'
 }
 
-const baseUrl = `https://${NEWS_RAPIDAPI_HOST}/`;
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/';
 
 const createRequest = (url) => ({url, headers: cryptoNewsApiHeaders});
 
