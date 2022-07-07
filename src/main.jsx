a465cf51be9d3894e6f06b0cfc5dcb96078532f6
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-07 22:51:10 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import App from './App'
+import store from './app/store';
+
+import 'antd/dist/antd.css';
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Router>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </Router>
+    </React.StrictMode>
+)
