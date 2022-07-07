d4f5e2fdd34b059ff901313b57a994a630849970
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-07 22:50:50 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+    Routes, Route,
+} from 'react-router-dom';
+import {
+    Layout,
+} from 'antd';
+
+import {Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News, Footer} from './components';
+import './App.css';
+const App = () => {
+    return (
+        <div className={"app"}>
+            <div className={"gradient-bg-welcome"}>
+                <Navbar/>
+            </div>
+
+            <div className={"main"}>
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path={"/"} element={<Homepage/>}/>
+                            <Route exact path={"/exchanges"} element={<Exchanges/>}/>
+                            <Route exact path={"/cryptocurrencies"} element={<Cryptocurrencies/>}/>
+                            <Route exact path={"/crypto/:coinId"} element={<CryptoDetails/>}/>
+                            <Route exact path={"/news"} element={<News/>}/>
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+
+            <div className={"gradient-bg-footer"}>
+                <Footer/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
