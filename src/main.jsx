3414bacbff9fbe56264c5868107a613d21dcd252
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-08 08:34:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Router basepath="/Cryptoverse/">
+        <Router basename="/Cryptoverse">
             <Provider store={store}>
                 <App/>
             </Provider>
