6b31abcf5d3e0cacbec40321d0ebb6bed13d984b
ABHISHEK-PANDEY2 <95560057+ABHISHEK-PANDEY2@users.noreply.github.com>
2022-07-16 16:19:46 +0530

    fix: Social Links (#31) (#39)
    * fix bug in social links
    
    * fix bug in social links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,20 +40,20 @@ const Navbar = () => {
             </nav>
             <div
                 className={"text-white md:flex hidden list-none flex-row justify-between items-center  text-white-600 hover:text-blue-800 visited:text-white-600 text-2xl"}>
-                <a className={"text-white"} href="https://twitter.com/CryptoverseWeb3"><i
+                <a className={"text-white"} href="https://twitter.com/CryptoverseWeb3" target={"_blank"}><i
                     className="social-icons ri-twitter-line"></i></a>
-                <a className={"text-white"} href="https://github.com/CryptoverseWeb3"><i
+                <a className={"text-white"} href="https://github.com/CryptoverseWeb3" target={"_blank"}><i
                     className="social-icons ri-github-fill"></i></a>
                 <a className={"text-white"}
-                   href="https://www.youtube.com/channel/UCu7EGHHiNgEN2228BKD1pqg/?sub_confirmation=1"><i
+                   href="https://www.youtube.com/channel/UCu7EGHHiNgEN2228BKD1pqg/?sub_confirmation=1" target={"_blank"}><i
                     className="social-icons ri-youtube-fill"></i></a>
-                <a className={"text-white"} href="https://instagram.com/CryptoverseWeb3"><i
+                <a className={"text-white"} href="https://instagram.com/CryptoverseWeb3" target={"_blank"}><i
                     className="social-icons ri-instagram-fill"></i></a>
-                <a className={"text-white"} href="https://discord.gg/WhDry9FWE7"><i
+                <a className={"text-white"} href="https://discord.gg/WhDry9FWE7" target={"_blank"}><i
                     className="social-icons ri-discord-fill"></i></a>
-                <a className={"text-white"} href="https://t.me/cryptoverseweb3"><i
+                <a className={"text-white"} href="https://t.me/cryptoverseweb3" target={"_blank"}><i
                     className="social-icons ri-telegram-line"></i></a>
-                <a className={"text-white"} href="https://www.facebook.com/cryptoverseweb3"><i
+                <a className={"text-white"} href="https://www.facebook.com/cryptoverseweb3" target={"_blank"}><i
                     className="social-icons ri-facebook-fill"></i></a>
             </div>
             <div className={"flex relative"}>
